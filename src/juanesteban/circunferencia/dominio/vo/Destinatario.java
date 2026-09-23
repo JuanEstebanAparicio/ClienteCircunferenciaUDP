3d6778c55d728a5a37c8d3e87d3cfe5dcd312d66
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2026-09-23 14:01:52 -0500

    vo creado

---
+++ src/juanesteban/circunferencia/dominio/vo/Destinatario.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juanesteban.circunferencia.dominio.vo;
+
+import java.util.Objects;
+import juanesteban.circunferencia.dominio.excepciones.DestinatarioIncorrectoException;
+import juanesteban.circunferencia.dominio.excepciones.PuertoIncorrectoException;
+/**
+ *
+ * @author apari
+ */
+public record Destinatario(String ip, int puerto) {
+    
+    public Destinatario {
+        if (Objects.isNull(ip) || ip.isBlank()) {
+            throw new DestinatarioIncorrectoException(
+            "La direccion IP del servidor no puede ser nula ni vacia.");
+        }
+        if (puerto <= 0 || puerto > 65535) {
+            throw new PuertoIncorrectoException("El puerto del servidor debe ser un numero entre 1 y 65535"); 
+        }
+        ip = ip.trim();
+    }
+    public String endpoint() {
+        return ip + ":" + puerto;
+    }
+}
---
+++ src/juanesteban/circunferencia/dominio/vo/Radio.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juanesteban.circunferencia.dominio.vo;
+
+import juanesteban.circunferencia.dominio.excepciones.RadioIncorrectoException;
+/**
+ *
+ * @author apari
+ */
+public record Radio(double valor) {
+    
+    private static final String MENSAJE_ERROR = "Radio incorrecto: debe ser mayor que 0";
+    
+    public Radio {
+        if (!Double.isFinite(valor) || valor <= 0) {
+            throw new RadioIncorrectoException(MENSAJE_ERROR);
+        } 
+    }
+}
